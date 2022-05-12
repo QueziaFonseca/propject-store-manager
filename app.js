19913c06f32f33c2d44412639eb424f5bf36da98
quezia-lima <qfonseca1@gmail.com>
2022-05-12 10:41:46 -0300

    3 - validations not working

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const productController = require('./controllers/productController ');
 const saleController = require('./controllers/saleController');
-// const productValidation = require('./middlewares/productValidation');
+const productValidation = require('./middlewares/productValidation');
+const saleValidation = require('./middlewares/saleValidation');
 
 const app = express();
 app.use(express.json());
-
-app.get('/products', productController.getAll);
-app.post('/products', productController.createProduct);
-app.get('/products/:id', productController.getById);
-
-app.get('/sales', saleController.getAll);
-app.get('/sales/:id', saleController.getById);
-
+// app.use('./products');
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.get('/products', productController.getAll);
+app.post('/products', productValidation);
+app.get('/products/:id', productController.getById);
+
+app.get('/sales', saleController.getAll);
+app.get('/sales/:id', saleController.getById);
+app.post('/sales', saleValidation);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
